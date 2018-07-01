d80a6319d8006db6c78ec6ac8cef5c90584423e3
Mehdi <medev.kym@gmail.com>
2018-07-01 15:52:46 +0200

    Implement the methods in CurrencyViewController to display them into the view

--- LeBaluchon/Controller/CurrencyViewController.swift
+++ LeBaluchon/Controller/CurrencyViewController.swift
@@ -14,10 +14,12 @@ class CurrencyViewController: UIViewController {
     @IBOutlet weak var currencyResultLabel: UILabel!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var convertBtn: UIButton!
+    @IBOutlet weak var baseCurrencyLabel: UILabel!
+    @IBOutlet weak var targetCurrencyLabel: UILabel!
     
     // MARK: Action
     @IBAction func convert() {
-        
+        getCalculatedCurrency()
     }
     
     // MARK: Methods
@@ -25,17 +27,77 @@ class CurrencyViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    /// Get data from the Model
+    private func getCalculatedCurrency() {
+        CurrencyService.sharedInstance.getCurrency { (success, currency) in
+            if success, let currency = currency {
+                self.updateDisplay(currency: currency)
+            } else {
+                self.alertMessage(title: "Probléme réseau", message: "Veuilliez vérifier votre connexion ou réessayer ultérieurement.")
+            }
+        }
+    }
+    
+    /// Update diplay of some labels
+    ///
+    /// - Parameter currency: Currency get from the Model to update into the view
+    private func updateDisplay(currency: Currency) {
+        guard let inputCurrency = currencyTextField.text else {
+            alertMessage(title: "Données manquante", message: "Merci de bien vouloir entrer une donnée !")
+            resetDisplay()
+            return
+        }
+        
+        guard let baseCurrencyRate = Double(inputCurrency) else {
+            alertMessage(title: "Données Erronées", message: "Merci de bien vouloir entrer un nombre pour obtenir la devise !")
+            resetDisplay()
+            return
+        }
+        
+        guard let rateTarget = currency.rates?.usd else {
+            alertMessage(title: "Taux indisponible", message: "Le taux n'a pu être obtenu du serveur")
+            return
+        }
+        
+        let currencyResult = Currency.calculateCurrency(baseCurrency: baseCurrencyRate, rateTarget: rateTarget)
+
+        currencyResultLabel.text = String(format: "%.3f", currencyResult)
+        baseCurrencyLabel.text = currency.baseCurrency
+        targetCurrencyLabel.text = "USD"
+    }
+    
+    /// Reset views that needs a default view in certain situations
+    private func resetDisplay() {
+        currencyTextField.text = ""
+        currencyResultLabel.text = ""
+    }
+    
+    /// Display pop up to warn the user
+    ///
+    /// - Parameters:
+    ///   - title: Alert title
+    ///   - message: Message title
+    private func alertMessage(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+}
+
+// MARK: Keyboard Behaviours
+extension CurrencyViewController: UITextFieldDelegate {
+    
     //Dismiss the keyboard when touching somewhere in the sreen
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
     }
-}
-
-// MARK: Keyboard return key
-extension CurrencyViewController: UITextFieldDelegate {
+    
+    //Set the keyboard return key in case changing the keyboard type
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         view.endEditing(true)
-//        Add method to convert currency
+        getCalculatedCurrency()
         return true
     }
     

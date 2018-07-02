73a23d38fb23c429ac717654165f03214c550153
Mehdi <medev.kym@gmail.com>
2018-07-02 11:12:51 +0200

    Add Currency.json & FakeResponseData.swift

---
+++ LeBaluchonTests/FakeResponseData.swift
@@ -0,0 +1,13 @@
+//
+//  FakeResponseData.swift
+//  LeBaluchonTests
+//
+//  Created by Mehdi on 02/07/2018.
+//  Copyright © 2018 Mehdi. All rights reserved.
+//
+
+import Foundation
+
+class FakeResponseData {
+    
+}

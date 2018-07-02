f658210a3676fa47dca5f7cfbdf459d842277415
Mehdi <medev.kym@gmail.com>
2018-07-02 11:39:02 +0200

    Implement FakeReponseData

--- LeBaluchonTests/FakeResponseData.swift
+++ LeBaluchonTests/FakeResponseData.swift
@@ -9,5 +9,21 @@
 import Foundation
 
 class FakeResponseData {
+    // MARK: Fake Data
+    static var currencyCorrectData: Data {
+        let bundle = Bundle(for: FakeResponseData.self)
+        let url = bundle.url(forResource: "Currency", withExtension: "json")
+        let data = try! Data(contentsOf: url!)
+        return data
+    }
     
+    static let currencyIncorrectData = "incorrectData".data(using: .utf8)
+    
+    // MARK: Fake Responses
+    let reponseOK = HTTPURLResponse(url: URL(string: "https://www.google.fr")!, statusCode: 200, httpVersion: nil, headerFields: nil)!
+    let reponseKO = HTTPURLResponse(url: URL(string: "https://www.google.fr")!, statusCode: 500, httpVersion: nil, headerFields: nil)!
+    
+    // MARK: Fake Error
+    class CurrencyError: Error {}
+    static let error = CurrencyError()
 }

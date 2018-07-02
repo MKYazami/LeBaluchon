16dd93df4485dff14c6c6093a781b7862418c6b3
Mehdi <medev.kym@gmail.com>
2018-07-02 12:33:32 +0200

    Preparation of the CurrencyService class to be tested

--- LeBaluchon/Model/Currency/CurrencyService.swift
+++ LeBaluchon/Model/Currency/CurrencyService.swift
@@ -13,22 +13,32 @@ class CurrencyService {
     static let sharedInstance = CurrencyService()
     private init() {}
     
-    // MARK: URL Session & Task properties
-    private let url = URL(string: "http://data.fixer.io/api/latest?access_key=f6ce7945bcfdbe9f45515b27e53ae84f&symbols=usd")!
+    // currencySession is in global scope to allows the dependency injection for tests
+    private var currencySession = URLSession(configuration: .default)
     
-    private var session = URLSession(configuration: .default)
-    
-    private var task: URLSessionDataTask?
+    /// This initializer is used only for testing
+    ///
+    /// - Parameter currencySession: Inject the URLSessionFake
+    init(currencySession: URLSession) {
+        self.currencySession = currencySession
+    }
     
     // MARK: Method
     
     /// Get currency from remote API
+    ///
+    /// - Parameter callBack: if any problem in the request callBack = (false, nil) if evrything Ok callBack = (true, currency)
     func getCurrency(callBack: @escaping (Bool, Currency?) -> Void) {
+        
+        // Set url for Session
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=f6ce7945bcfdbe9f45515b27e53ae84f&symbols=usd")!
+        
+        var task: URLSessionDataTask?
         
         //Cancel the task before to start new one
         task?.cancel()
         
-        task = session.dataTask(with: url) { (data, response, error)  in
+        task = currencySession.dataTask(with: url) { (data, response, error)  in
             //Bring to main thread
             DispatchQueue.main.async {
                 //Check data and no error

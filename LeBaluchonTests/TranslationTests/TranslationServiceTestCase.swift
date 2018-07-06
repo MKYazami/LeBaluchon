4901a0d86c3f9dc5d13ca4b9aba068aacd743909
Mehdi <medev.kym@gmail.com>
2018-07-06 15:12:56 +0200

    Add DataTranslation.json & TranslationServiceTestCase to test target

---
+++ LeBaluchonTests/TranslationTests/TranslationServiceTestCase.swift
@@ -0,0 +1,20 @@
+//
+//  TranslationServiceTestCase.swift
+//  LeBaluchonTests
+//
+//  Created by Mehdi on 06/07/2018.
+//  Copyright © 2018 Mehdi. All rights reserved.
+//
+
+import XCTest
+
+class TranslationServiceTestCase: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    
+    
+}
